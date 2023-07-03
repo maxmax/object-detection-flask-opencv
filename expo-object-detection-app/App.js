254865c11e9da35d8d652b1cda2d06c91460eb8d
maxmax <maxbakaiev@gmail.com>
2023-07-03 20:46:40 +0300

    expo

---
+++ expo-object-detection-app/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.videoDetection}
+        source={{uri: 'http://127.0.0.1:5000/video_feed'}}
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  videoDetection: {
+    width: 400,
+    height: 300,
+  },
+});
